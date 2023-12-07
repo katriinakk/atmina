5eba7d83d6c5a4e9c108d0433394cc2e9f625df9
katriinakk <123467252+katriinakk@users.noreply.github.com>
2023-12-07 15:00:44 +0200

    1.stunda

---
+++ Atmina.py
@@ -0,0 +1,33 @@
+from tkinter import *
+#from PIL import ImageTk
+logs=Tk()
+logs.title("Vienādie attēli")
+
+
+btn0=Button(width=20, height = 10)
+btn1=Button(width=20, height = 10)
+btn2=Button(width=20, height = 10)
+btn3=Button(width=20, height = 10)
+btn4=Button(width=20, height = 10)
+btn5=Button(width=20, height = 10)
+btn6=Button(width=20, height = 10)
+btn7=Button(width=20, height = 10)
+btn8=Button(width=20, height = 10)
+btn9=Button(width=20, height = 10)
+btn10=Button(width=20, height =10)
+btn11=Button(width=20, height = 10)
+
+btn0.grid(row=1, column=1)
+btn1.grid(row=1, column=2)
+btn2.grid(row=1, column=3)
+btn3.grid(row=1, column=4)
+btn4.grid(row=2, column=1)
+btn5.grid(row=2, column=2)
+btn6.grid(row=2, column=3)
+btn7.grid(row=2, column=4)
+btn8.grid(row=3, column=1)
+btn9.grid(row=3, column=2)
+btn10.grid(row=3, column=3)
+btn11.grid(row=3, column=4)
+
+logs.mainloop()
