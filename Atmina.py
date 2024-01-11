050bb7f7b42ac4d3369545b2ed483bfce4ca90d3
katriinakk <123467252+katriinakk@users.noreply.github.com>
2024-01-11 15:06:53 +0200

    Update Atmina.py

--- Atmina.py
+++ Atmina.py
@@ -2,10 +2,44 @@ from tkinter import *
 from PIL import ImageTk, Image
 import random
 from tkinter import messagebox
+
+
+
+
+
 logs=Tk()
 logs.title("Vienādie attēli")
 
 bgimg=ImageTk.PhotoImage(Image.open("77.jpg"))
+
+
+
+def click(btn, number):
+    global count, correct, answers, answersD
+    if btn["image"] == "pyimage1" and count<2:
+        btn["image"] == imglist[number]
+        count+=1
+        answers.append(number)
+        answersD[btn]=imglist[number]
+    if len(answers)==2:
+        if imglist[answers[0]]==imglist[answers[1]]: #salīdzina attēlus, kas saglabāts vārdnīcā ar attēl sarakstu
+            for key in answersD:
+                key["state"]=DISABLED
+            correct+=2
+            if correct==2:
+                messagebox.showinfo("uzminēji")
+                correct=0
+        else:
+            #messagebox.showinfo("neuzminēji")
+            for key in answersD:
+                key["image"]="pyimage1"
+        count=0
+        answers=[]
+        answersD={}
+    return 0
+
+
+
 
 
 img1=ImageTk.PhotoImage(Image.open("1.png"))
@@ -15,8 +49,10 @@ img4=ImageTk.PhotoImage(Image.open("4.png"))
 img5=ImageTk.PhotoImage(Image.open("5.png"))
 img6=ImageTk.PhotoImage(Image.open("6.png"))
 
-imglist=[img1, img2, img3, img4, img5, img6, img1, img2, img3, img4, img5, img6]
+imglist=[img1, img1, img2, img2, img3, img3, img4, img4, img5, img5, img6, img6]
 random.shuffle(imglist)
+
+
 
 count = 0
 correct = 0
@@ -48,44 +84,17 @@ btn11=Button(width=200, height = 200, image = bgimg, bg=("salmon"), command=lamb
 #
 #
 
-def click(btn, number):
-    global count, correct, answers, answersD
-    if btn["image"] == "pyimage6" and count<2:
-        btn["image"] == imglist[number]
-        count+=1
-        answers.append(number)
-        answersD[btn]=imglist[number]
-    if len(answers)==2:
-        if imglist[answers[0]]==imglist[answers[1]]: #salīdzina attēlus, kas saglabāts vārdnīcā ar attēl sarakstu
-            for key in answersD:
-                key["state"]=DISABLED
-            correct+=2
-            if correct==2:
-                messagebox.showinfo("uzminēji")
-                correct=0
-        else:
-            messagebox.showinfo("neuzminēji")
-            for key in answersD:
-                key["image"]="pyimage6"
-        count=0
-        answers=[]
-        answersD={}
-    return 0
-
-
-
-
-btn0.grid(row=1, column=1)
-btn1.grid(row=1, column=2)
-btn2.grid(row=1, column=3)
-btn3.grid(row=1, column=4)
-btn4.grid(row=2, column=1)
-btn5.grid(row=2, column=2)
-btn6.grid(row=2, column=3)
-btn7.grid(row=2, column=4)
-btn8.grid(row=3, column=1)
-btn9.grid(row=3, column=2)
-btn10.grid(row=3, column=3)
-btn11.grid(row=3, column=4)
+btn0.grid(row=0, column=0)
+btn1.grid(row=0, column=1)
+btn2.grid(row=0, column=2)
+btn3.grid(row=0, column=3)
+btn4.grid(row=1, column=0)
+btn5.grid(row=1, column=1)
+btn6.grid(row=1, column=2)
+btn7.grid(row=1, column=3)
+btn8.grid(row=2, column=0)
+btn9.grid(row=2, column=1)
+btn10.grid(row=2, column=2)
+btn11.grid(row=2, column=3)
 
 logs.mainloop()
